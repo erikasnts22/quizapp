b066062392719f72d1d91fd0ff78b7588e33e2c5
“erikasnts22” <sntserika22@gmail.com>
2022-01-24 14:55:04 +0800

    display feedback for correct & incorrect answers

--- game.js
+++ game.js
@@ -43,13 +43,12 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions  = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 }
 
 getNewQuestion = () => {
 
-    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         return window.location.assign('/end.html');
     }
 
@@ -75,8 +74,16 @@ choices.forEach( choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+       
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+          }, 1000);
+
     });
 });
 
